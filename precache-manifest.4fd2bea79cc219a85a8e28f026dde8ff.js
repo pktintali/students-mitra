b0d7341710096480912c5b63caa3fe86c1d9ae49
* <4gpradeep@gmail.com>
2020-08-28 22:18:49 +0530

    Updates

---
+++ precache-manifest.4fd2bea79cc219a85a8e28f026dde8ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87bd2dad1cbf67dea1889ad6ca4de947",
+    "url": "/webapp/index.html"
+  },
+  {
+    "revision": "98863d01c237a8b1e43a",
+    "url": "/webapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "44efbeb1a345ba240663",
+    "url": "/webapp/static/js/2.6b633340.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/webapp/static/js/2.6b633340.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98863d01c237a8b1e43a",
+    "url": "/webapp/static/js/main.7126ce5d.chunk.js"
+  },
+  {
+    "revision": "3589d18c7166a78ad2eb",
+    "url": "/webapp/static/js/runtime-main.cad45c34.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/webapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
