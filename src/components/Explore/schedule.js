b3b846344ad30a2c5378fa6ed9c488dd5e751dab
* <4gpradeep@gmail.com>
2020-10-20 14:13:44 +0530

    time table added

--- src/components/Explore/schedule.js
+++ src/components/Explore/schedule.js
@@ -5,51 +5,66 @@ import TopBar from '../TopBar';
 function Schedule() {
 
 var d = new Date()
-var day = d.getDay()
+const day = d.getDay()
+
+const lunch = 'https://www.google.com/search?q=food+near+me'
+const coi = 'https://chat.whatsapp.com/'
+const cd = 'https://chat.whatsapp.com/'
+const cdlab ='http://moodleglbitm.live:9091/course/view.php?id=187'
+const mp = 'https://chat.whatsapp.com/' 
+const dbms ='https://chat.whatsapp.com/'
+const daa = 'https://chat.whatsapp.com/'
+const da = 'http://moodleglbitm.live:9091/course/view.php?id=209'
+const ml = 'http://moodleglbitm.live:9091/course/view.php?id=215'
+const ttc1 ='https://meet.google.com/hee-mrfc-ewb'
+const ttc2 = 'meet.google.com/jxo-wwxw-zej'
+const pdp = 'http://lms.glbitm.org:9099/moodle/course/view.php?id=50#section-0'
+const daaquiz = 'http://moodleglbitm.live:9091/course/view.php?id=192'
 
 if(day===1){
 return (
       <div className = 'w3-container'>
+         <a href = {coi} target ='blank'>
           <div className ='w3-container w3-card'>
-             <p className = 'w3-left'>9:30-10:20 AM</p>
-             <h3 className = 'w3-right'>NC+</h3>
+             <p className = 'w3-right'>9:30-10:20 AM</p>
+             <h3 className = 'w3-left'>NC+</h3>
            </div>
-           <br></br>
+           </a><br></br><a href = {da} target ='blank'>
            <div className ='w3-container w3-card'>
-             <p className = 'w3-left'>10:30-11:20 AM</p>
-             <h3 className = 'w3-right'>DA</h3>
+             <p className = 'w3-right'>10:30-11:20 AM</p>
+             <h3 className = 'w3-left'>DA</h3>
            </div>
-           <br></br>
+           </a><br></br><a href = {daa} target ='blank'>
            <div className ='w3-container w3-card'>
-             <p className = 'w3-left'>11:30 AM-12:20 PM</p>
-             <h3 className = 'w3-right'>DAA</h3>
+             <p className = 'w3-right'>11:30 AM-12:20 PM</p>
+             <h3 className = 'w3-left'>DAA</h3>
            </div>
-           <br></br>
+           </a><br></br><a href = {dbms} target ='blank'>
            <div className ='w3-container w3-card'>
-             <p className = 'w3-left'>12:30-1:20 PM</p>
-             <h3 className = 'w3-right'>DBMS</h3>
+             <p className = 'w3-right'>12:30-1:20 PM</p>
+             <h3 className = 'w3-left'>DBMS</h3>
            </div>
-           <br></br>
+           </a><br></br><a href = {lunch} target ='blank'>
            <div className ='w3-container w3-card w3-green'>
-             <p className = 'w3-left'>1:20-2:00 PM</p>
-             <h3 className = 'w3-right'>Enjoy</h3>
+             <p className = 'w3-right'>1:20-2:00 PM</p>
+             <h3 className = 'w3-left'>Lunch</h3>
            </div>
-           <br></br>
+           </a><br></br><a href = {daa} target ='blank'>
            <div className ='w3-container w3-card'>
-             <p className = 'w3-left'>2:00-2:50 PM</p>
-             <h3 className = 'w3-right'>DAA Lab</h3>
+             <p className = 'w3-right'>2:00-2:50 PM</p>
+             <h3 className = 'w3-left'>DAA Lab</h3>
            </div>
-           <br></br>
+           </a><br></br><a href = {ml} target ='blank'>
            <div className ='w3-container w3-card'>
-             <p className = 'w3-left'>3:00-3:50 PM</p>
-             <h3 className = 'w3-right'>MLT</h3>
+             <p className = 'w3-right'>3:00-3:50 PM</p>
+             <h3 className = 'w3-left'>MLT</h3>
            </div>
-           <br></br>
+           </a><br></br><a href = {da} target ='blank'>
            <div className ='w3-container w3-card'>
-             <p className = 'w3-left'>4:00-4:50 PM</p>
-             <h3 className = 'w3-right'>DA Quiz</h3>
+             <p className = 'w3-right'>4:00-4:50 PM</p>
+             <h3 className = 'w3-left'>DA Quiz</h3>
            </div>
-           <br></br>
+           </a><br></br>
      </div>
   );
 }
@@ -57,82 +72,84 @@ else if(day===2)
 {
 return (
       <div className = 'w3-container'>
+      <a href = {cd} target ='blank'>
           <div className ='w3-container w3-card'>
-             <p className = 'w3-left'>9:30-10:20 AM</p>
-             <h3 className = 'w3-right'>CD</h3>
+             <p className = 'w3-right'>9:30-10:20 AM</p>
+             <h3 className = 'w3-left'>CD</h3>
            </div>
-           <br></br>
+           </a><br></br><a href = {da} target ='blank'>
            <div className ='w3-container w3-card'>
-             <p className = 'w3-left'>10:30-11:20 AM</p>
-             <h3 className = 'w3-right'>DA</h3>
+             <p className = 'w3-right'>10:30-11:20 AM</p>
+             <h3 className = 'w3-left'>DA</h3>
            </div>
-           <br></br>
+           </a><br></br><a href = {ml} target ='blank'>
            <div className ='w3-container w3-card'>
-             <p className = 'w3-left'>11:30 AM-12:20 PM</p>
-             <h3 className = 'w3-right'>MLT</h3>
+             <p className = 'w3-right'>11:30 AM-12:20 PM</p>
+             <h3 className = 'w3-left'>MLT</h3>
            </div>
-           <br></br>
-           <div className ='w3-container w3-card'>
-             <p className = 'w3-left'>12:30-1:20 PM</p>
-             <h3 className = 'w3-right'>PDP</h3>
+           </a><br></br>
+           <a target = 'blank' href = {pdp}><div className ='w3-container w3-card'>
+             <p className = 'w3-right'>12:30-1:20 PM</p>
+             <h3 className = 'w3-left'>PDP</h3>
            </div>
-           <br></br>
+           </a><br></br><a href = {lunch} target ='blank'>
            <div className ='w3-container w3-card w3-green'>
-             <p className = 'w3-left'>1:20-2:00 PM</p>
-             <h3 className = 'w3-right'>Enjoy</h3>
+             <p className = 'w3-right'>1:20-2:00 PM</p>
+             <h3 className = 'w3-left'>Lunch</h3>
            </div>
-           <br></br>
+           </a><br></br><a href = {ttc1} target ='blank'>
            <div className ='w3-container w3-card'>
-             <p className = 'w3-left'>2:00-3:50 PM</p>
-             <h3 className = 'w3-right'>TTC</h3>
+             <p className = 'w3-right'>2:00-3:50 PM</p>
+             <h3 className = 'w3-left'>TTC</h3>
            </div>
-           <br></br>
+           </a><br></br><a href = {daaquiz} target ='blank'>
            <div className ='w3-container w3-card'>
-             <p className = 'w3-left'>4:00-4:50 PM</p>
-             <h3 className = 'w3-right'>DAA Quiz</h3>
+             <p className = 'w3-right'>4:00-4:50 PM</p>
+             <h3 className = 'w3-left'>DAA Quiz</h3>
            </div>
-           <br></br>
+           </a><br></br>
      </div>
   );
 }
 else if(day===3){
 return (
       <div className = 'w3-container'>
+      <a href = {dbms} target ='blank'>
           <div className ='w3-container w3-card'>
-             <p className = 'w3-left'>9:30-10:20 AM</p>
-             <h3 className = 'w3-right'>DBMS</h3>
+             <p className = 'w3-right'>9:30-10:20 AM</p>
+             <h3 className = 'w3-left'>DBMS</h3>
            </div>
-           <br></br>
+           </a><br></br><a href = {daa} target ='blank'>
            <div className ='w3-container w3-card'>
-             <p className = 'w3-left'>10:30-11:20 AM</p>
-             <h3 className = 'w3-right'>DAA</h3>
+             <p className = 'w3-right'>10:30-11:20 AM</p>
+             <h3 className = 'w3-left'>DAA</h3>
            </div>
-           <br></br>
+           </a><br></br><a href = {ttc2} target ='blank'>
            <div className ='w3-container w3-card'>
-             <p className = 'w3-left'>11:30 AM-01:20 PM</p>
-             <h3 className = 'w3-right'>TTC</h3>
+             <p className = 'w3-right'>11:30 AM-01:20 PM</p>
+             <h3 className = 'w3-left'>TTC</h3>
            </div>
-           <br></br>
+           </a><br></br><a href = {lunch} target ='blank'>
            <div className ='w3-container w3-card w3-green'>
-             <p className = 'w3-left'>1:20-2:00 PM</p>
-             <h3 className = 'w3-right'>Enjoy</h3>
+             <p className = 'w3-right'>1:20-2:00 PM</p>
+             <h3 className = 'w3-left'>Lunch</h3>
            </div>
-           <br></br>
+           </a><br></br><a href = {cdlab} target ='blank'>
            <div className ='w3-container w3-card'>
-             <p className = 'w3-left'>2:00-2:50 PM</p>
-             <h3 className = 'w3-right'>CD Lab</h3>
+             <p className = 'w3-right'>2:00-2:50 PM</p>
+             <h3 className = 'w3-left'>CD Lab</h3>
            </div>
-           <br></br>
+           </a><br></br><a href = {da} target ='blank'>
            <div className ='w3-container w3-card'>
-             <p className = 'w3-left'>3:00-3:50 PM</p>
-             <h3 className = 'w3-right'>DA</h3>
+             <p className = 'w3-right'>3:00-3:50 PM</p>
+             <h3 className = 'w3-left'>DA</h3>
            </div>
-           <br></br>
+           </a><br></br><a href = {coi} target ='blank'>
            <div className ='w3-container w3-card'>
-             <p className = 'w3-left'>4:00-4:50 PM</p>
-             <h3 className = 'w3-right'>NC+ Quiz</h3>
+             <p className = 'w3-right'>4:00-4:50 PM</p>
+             <h3 className = 'w3-left'>NC+ Quiz</h3>
            </div>
-           <br></br>
+           </a><br></br>
      </div>
   );
 }
@@ -140,41 +157,42 @@ return (
 else if(day===4){
 return (
       <div className = 'w3-container'>
+      <a href = {coi} target ='blank'>
           <div className ='w3-container w3-card'>
-             <p className = 'w3-left'>9:30-10:20 AM</p>
-             <h3 className = 'w3-right'>NC+</h3>
+             <p className = 'w3-right'>9:30-10:20 AM</p>
+             <h3 className = 'w3-left'>NC+</h3>
            </div>
-           <br></br>
+           </a><br></br><a href = {dbms} target ='blank'>
            <div className ='w3-container w3-card'>
-             <p className = 'w3-left'>10:30-11:20 AM</p>
-             <h3 className = 'w3-right'>DBMS Quiz</h3>
+             <p className = 'w3-right'>10:30-11:20 AM</p>
+             <h3 className = 'w3-left'>DBMS Quiz</h3>
            </div>
-           <br></br>
+           </a><br></br><a href = {pdp} target ='blank'>
            <div className ='w3-container w3-card'>
-             <p className = 'w3-left'>11:30 AM-01:20 PM</p>
-             <h3 className = 'w3-right'>PDP Training</h3>
+             <p className = 'w3-right'>11:30 AM-01:20 PM</p>
+             <h3 className = 'w3-left'>PDP Training</h3>
            </div>
-           <br></br>
+           </a><br></br><a href = {lunch} target ='blank'>
            <div className ='w3-container w3-card w3-green'>
-             <p className = 'w3-left'>1:20-2:00 PM</p>
-             <h3 className = 'w3-right'>Enjoy</h3>
+             <p className = 'w3-right'>1:20-2:00 PM</p>
+             <h3 className = 'w3-left'>Lunch</h3>
            </div>
-           <br></br>
+           </a><br></br><a href = {cd} target ='blank'>
            <div className ='w3-container w3-card'>
-             <p className = 'w3-left'>2:00-2:50 PM</p>
-             <h3 className = 'w3-right'>CD</h3>
+             <p className = 'w3-right'>2:00-2:50 PM</p>
+             <h3 className = 'w3-left'>CD</h3>
            </div>
-           <br></br>
+           </a><br></br><a href = {dbms} target ='blank'>
            <div className ='w3-container w3-card'>
-             <p className = 'w3-left'>3:00-3:50 PM</p>
-             <h3 className = 'w3-right'>DBMS Lab</h3>
+             <p className = 'w3-right'>3:00-3:50 PM</p>
+             <h3 className = 'w3-left'>DBMS Lab</h3>
            </div>
-           <br></br>
+           </a><br></br><a href = {ml} target ='blank'>
            <div className ='w3-container w3-card'>
-             <p className = 'w3-left'>4:00-4:50 PM</p>
-             <h3 className = 'w3-right'>MLT Quiz</h3>
+             <p className = 'w3-right'>4:00-4:50 PM</p>
+             <h3 className = 'w3-left'>MLT Quiz</h3>
            </div>
-           <br></br>
+           </a><br></br>
      </div>
   );
 }
@@ -182,56 +200,57 @@ return (
 else if(day===5){
 return (
       <div className = 'w3-container'>
+      <a href = {daa} target ='blank'>
           <div className ='w3-container w3-card'>
-             <p className = 'w3-left'>9:30-10:20 AM</p>
-             <h3 className = 'w3-right'>DAA</h3>
+             <p className = 'w3-right'>9:30-10:20 AM</p>
+             <h3 className = 'w3-left'>DAA</h3>
            </div>
-           <br></br>
+           </a><br></br><a href = {cd} target ='blank'>
            <div className ='w3-container w3-card'>
-             <p className = 'w3-left'>10:30-11:20 AM</p>
-             <h3 className = 'w3-right'>CD</h3>
+             <p className = 'w3-right'>10:30-11:20 AM</p>
+             <h3 className = 'w3-left'>CD</h3>
            </div>
-           <br></br>
+           </a><br></br><a href = {dbms} target ='blank'>
            <div className ='w3-container w3-card'>
-             <p className = 'w3-left'>11:30 AM-12:20 PM</p>
-             <h3 className = 'w3-right'>DBMS</h3>
+             <p className = 'w3-right'>11:30 AM-12:20 PM</p>
+             <h3 className = 'w3-left'>DBMS</h3>
            </div>
-           <br></br>
+           </a><br></br><a href = {pdp} target ='blank'>
            <div className ='w3-container w3-card'>
-             <p className = 'w3-left'>12:30-1:20 PM</p>
-             <h3 className = 'w3-right'>PDP</h3>
+             <p className = 'w3-right'>12:30-1:20 PM</p>
+             <h3 className = 'w3-left'>PDP</h3>
            </div>
-           <br></br>
+           </a><br></br><a href = {lunch} target ='blank'>
            <div className ='w3-container w3-card w3-green'>
-             <p className = 'w3-left'>1:20-2:00 PM</p>
-             <h3 className = 'w3-right'>Enjoy</h3>
+             <p className = 'w3-right'>1:20-2:00 PM</p>
+             <h3 className = 'w3-left'>Lunch</h3>
            </div>
-           <br></br>
+           </a><br></br><a href = {ml} target ='blank'>
            <div className ='w3-container w3-card'>
-             <p className = 'w3-left'>2:00-2:50 PM</p>
-             <h3 className = 'w3-right'>MLT</h3>
+             <p className = 'w3-right'>2:00-2:50 PM</p>
+             <h3 className = 'w3-left'>MLT</h3>
            </div>
-           <br></br>
+           </a><br></br><a href = {mp} target ='blank'>
            <div className ='w3-container w3-card'>
-             <p className = 'w3-left'>3:00-3:50 PM</p>
-             <h3 className = 'w3-right'>MP</h3>
+             <p className = 'w3-right'>3:00-3:50 PM</p>
+             <h3 className = 'w3-left'>MP</h3>
            </div>
-           <br></br>
+           </a><br></br><a href = {cd} target ='blank'>
            <div className ='w3-container w3-card'>
-             <p className = 'w3-left'>4:00-4:50 PM</p>
-             <h3 className = 'w3-right'>CD Quiz</h3>
+             <p className = 'w3-right'>4:00-4:50 PM</p>
+             <h3 className = 'w3-left'>CD Quiz</h3>
            </div>
-           <br></br>
+           </a><br></br>
      </div>
   );
 }
 
 else if(day===6){
-return <h1>Enjoy Saturday</h1>;
+return <h1>Lunch Saturday</h1>;
 }
 
 else if(day===7){
-return <h1>Enjoy Sunday</h1>;
+return <h1>Lunch Sunday</h1>;
 }
 
 }
